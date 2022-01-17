8bde5667045fae8ba563339a17587562dbdaa483
Ben Schwenke <benschwenke@protonmail.com>
2022-01-17 16:24:18 -0600

    More mobile style fixes
    Update class names
    
    Add a bit more height to keyboard keys
    
    Added some code I ripped from css tricks for viewport calc on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { BoardState } from './models/board-state.interface';
 import { BoardStateService } from './services/board-state.service';
 import { DictionaryService } from './services/dictionary.service';
@@ -19,7 +19,12 @@ export class AppComponent {
     private sessionService: SessionService,
     private boardStateService: BoardStateService
   ) {
+    //  Ripped from https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
 
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
 
   ngOnInit(): void {
@@ -41,6 +46,12 @@ export class AppComponent {
 
       this.boardState = boardState;
     })
+  }
+
+  @HostListener('window:resize', ['$event'])
+  updateViewPortHeight(event: any): void {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
 
   // Ensure the user doesn't accidentally toggle the side nav after clicking options and then hitting ENTER to submit a guess
