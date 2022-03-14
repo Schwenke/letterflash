396ab529cf101fe413c2b556447c7c5f11308054
Ben Schwenke <benschwenke@protonmail.com>
2022-03-14 14:45:56 -0500

    Update stats
    Games now splits by word length
    
    Removed games won
    
    Removed # guesses. Silly.
    
    Added average game time
    
    Removed reference to PUZZLE. Confusing.
    
    Leave room at bottom for future add.

--- src/app/components/stats/stats/stats.component.ts
+++ src/app/components/stats/stats/stats.component.ts
@@ -19,6 +19,14 @@ export class StatsComponent implements OnInit {
     })
   }
 
+  getGamesPlayed(): string {
+    let fiveLetterGames = this.stats.played_5 ? this.stats.played_5 : 0;
+    let sixLetterGames = this.stats.played_6 ? this.stats.played_6 : 0;
+    let sevenLetterGames = this.stats.played_7 ? this.stats.played_7 : 0;
+
+    return `${fiveLetterGames} / ${sixLetterGames} / ${sevenLetterGames}`;
+  }
+
   getTotalGames(): number {
     let totalGamesPlayed = this.stats.played_5 + this.stats.played_6 + this.stats.played_7;
 
@@ -60,6 +68,25 @@ export class StatsComponent implements OnInit {
     let hours   = Math.floor(totalSeconds / 3600); // get hours
     let minutes = Math.floor((totalSeconds - (hours * 3600)) / 60); // get minutes
     let seconds = totalSeconds - (hours * 3600) - (minutes * 60); //  get seconds
+
+    let hourString = hours < 10 ? `0${hours}` : `${hours}`;
+    let minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    let secondString = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${hourString}:${minuteString}:${secondString}`;
+  }
+
+  getAverageGameTime(): string {
+    let totalSeconds = this.stats.time;
+    let gamesPlayed = this.getTotalGames();
+
+    if (!totalSeconds || totalSeconds === 0 || gamesPlayed === 0) return "00:00:00";
+
+    let averageSeconds = Math.floor(totalSeconds / gamesPlayed);
+
+    let hours   = Math.floor(averageSeconds / 3600); // get hours
+    let minutes = Math.floor((averageSeconds - (hours * 3600)) / 60); // get minutes
+    let seconds = averageSeconds - (hours * 3600) - (minutes * 60); //  get seconds
 
     let hourString = hours < 10 ? `0${hours}` : `${hours}`;
     let minuteString = minutes < 10 ? `0${minutes}` : `${minutes}`;
